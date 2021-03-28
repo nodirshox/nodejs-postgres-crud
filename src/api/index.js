17981d191cd0dee17e3c226b68815c05e26e2120
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2021-03-28 23:19:01 +0500

    Added get one.

--- src/api/index.js
+++ src/api/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 // contacts
 router.get("/contact", contactStorage.find);
 router.post("/contact", contactStorage.create);
+router.get("/contact/:contact_id", contactStorage.get);
+//router.put("/contact/:contact_id", contactStorage.update);
+//router.delete("/contact/:contact_id", contactStorage.get);
 
 module.exports = router;
--- src/storage/postgres/contact.js
+++ src/storage/postgres/contact.js
@@ -8,7 +8,7 @@ let contactStorage = {
                 "INSERT INTO contacts (first_name, last_name, email, phone) VALUES ($1, $2, $3, $4) RETURNING id",
                 [newContact.first_name, newContact.last_name, newContact.email, newContact.phone]
             )
-            
+
             return res.status(200).send({
                 id: newContact.rows[0].id
             });
@@ -27,7 +27,18 @@ let contactStorage = {
             });
         } catch (error) {
             throw new Error(error.message);
-
+        }
+    },
+    get: async (req, res) => {
+        try {
+            let contact = await db.query(
+                "SELECT * FROM contacts WHERE id = $1", [req.params.contact_id]
+            )
+            return res.status(200).json({
+                contact: contact.rows[0]
+            })
+        } catch (error) {
+            throw new Error(error.message);
         }
     }
 }
